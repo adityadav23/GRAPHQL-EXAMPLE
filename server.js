8288b857c774ac8791eb72e0d6b2ec7bbe0fe91a
Aditya Yadav <adityadav023>
2022-03-13 03:19:47 +0530

    using graphQL in node

--- server.js
+++ server.js
@@ -4,9 +4,24 @@ const {graphqlHTTP} = require('express-graphql')
 
 const PORT = process.env.PORT || 3000
 
+const schema = buildSchema(`
+    type Query{
+        description: String,
+        price: Float
+    }`
+)   
+
+const root = {
+    description: "red shoe",
+    price: 23.21
+}
+
 const app = express()
 
-
+app.use('/graphql',graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+}))
 app.listen(PORT, ()=>{
     console.log(`Graphql server running on port ${PORT}...`)
 })
