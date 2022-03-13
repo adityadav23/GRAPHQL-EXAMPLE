90edaf62f206b418d7cbfe85d91adb33df2ced9c
Aditya Yadav <adityadav023>
2022-03-13 09:40:27 +0530

    Build an e-commerce order history schema

--- server.js
+++ server.js
@@ -6,14 +6,66 @@ const PORT = process.env.PORT || 3000
 
 const schema = buildSchema(`
     type Query{
-        description: String,
-        price: Float
-    }`
+       products: [Product]
+       orders:[Order]
+    }
+    
+    type Product{
+        id: ID!
+        description: String!
+        reviews:[Review]
+        price: Float!
+    }
+
+    type Review{
+        rating: Int!
+        comment: String
+    }
+    type Order{
+        date: String!
+        subtotal: Float!
+        items:[OrderItem]
+    }
+
+    type OrderItem{
+        product: Product!
+        quantity:Int!
+    }
+
+
+    
+    `
 )   
 
 const root = {
-    description: "red shoe",
-    price: 23.21
+  products:[
+      {
+          id:'redshoe',
+          description: 'Red Shoe',
+          price: 123.2
+      },
+      {
+          id: 'bluejean',
+          description: 'Blue jeans',
+          price: 21.2
+      }
+  ],
+  orders:[{
+      date:'2005-05-05',
+      subtotal: 34.2,
+      items:[
+          {
+              product:{
+                id:'redshoe',
+                description: 'Old Red Shoe',
+                price: 13.2
+              },
+              quantity: 2
+          }
+      ]
+  }]
+
+
 }
 
 const app = express()
